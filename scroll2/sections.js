09e81d58ead5f53262c39d71e925ca9241f65c51
Kevin Clyne <kevin@clyne.com>
2020-05-14 05:31:12 -0400

    Update sections.js

--- scroll2/sections.js
+++ scroll2/sections.js
@@ -230,7 +230,8 @@ function drawInitial(){
             .style('left', (d3.event.pageX + 10)+ 'px')
             .style('top', (d3.event.pageY - 25) + 'px')
             .style('display', 'inline-block')
-            .html(`<strong>Occupation:</strong> ${d.Major[0] + d.Major.slice(1,).toLowerCase()}`)
+            .html(`<strong>Occupation:</strong> ${d.Major[0] + d.Major.slice(1,).toLowerCase()}
+                <br> <strong>Median Salary:</strong> $${d3.format(",.2r")(d.Median)}`)
     }
     
     function mouseOut(d, i){
