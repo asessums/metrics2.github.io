ec34c7ca22245e5f8a006b5de5fe53569847040a
Kevin Clyne <kevin@clyne.com>
2020-05-14 05:16:54 -0400

    Update sections.js

--- scroll2/sections.js
+++ scroll2/sections.js
@@ -632,7 +632,7 @@ function draw4(){
     svg.selectAll('.occs')
         .transition().duration(600).delay((d, i) => i * 2).ease(d3.easeBack)
             .attr('r', 5)
-            .attr('cx', d => histXScale(d.Median))
+            .attr('cx', d => histXScale(d.Midpoint))
             .attr('cy', d => histYScale(d.HistCol))
             .attr('fill', d => categoryColorScale(d.Category))
 
